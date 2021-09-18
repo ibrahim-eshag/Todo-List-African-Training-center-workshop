2f3a3d54db337df1a9c08d4749ae401b9f1ad4c8
Amged <hemaisne@yahoo.com>
2021-09-18 23:20:35 +0200

    Initial Commit

---
+++ Todo List African Training center completed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo List African Training center completed
+//
+//  Created by Amged on 9/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
